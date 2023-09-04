c350535f7e6e19e23e80008caf248b34e24d4d2a
AlessandroFacci <alessandro.facci1996@gmail.com>
2023-09-04 16:58:23 +0200

    index and js: add function changeActiveContact

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
+      activeContact: 0,
       contacts: [
         {
           name: "Michele",
@@ -167,5 +168,10 @@ createApp({
         },
       ],
     };
+  },
+  methods: {
+    changeActiveContact(index) {
+      this.activeContact = index;
+    },
   },
 }).mount("#app");
